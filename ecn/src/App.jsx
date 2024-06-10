3e086a2244daced47510b95512ddf9eb408dff01
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-06-10 14:00:12 +0530

    ADD FILE TO GIT

---
+++ ecn/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+     <h1 className='text-3xl font-b bg-red-400'>Quasim Khan</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ecn/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='3xl font-bold bg-slate-900'>Navbar</div>
+  )
+}
+
+export default Navbar
